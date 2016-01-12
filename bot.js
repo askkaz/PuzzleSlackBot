da09e567d884b70b9c36e253a94dd5081525deb8
Adam Kaczmarek <adamkaz@gmail.com>
2016-01-12 13:52:05 -0500

    initial HTTP listening

--- bot.js
+++ bot.js
@@ -65,8 +65,27 @@ This bot demonstrates many of the core features of Botkit:
 //AVOID HEROKU BINDING ERROR
 var http = require('http'); 
 http.createServer(function (req, res) { 
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.send('it is running\n'); 
+  if (req.method == 'POST') {
+        console.log("POST");
+        var body = '';
+        req.on('data', function (data) {
+            body += data;
+            console.log("Partial body: " + body);
+        });
+        req.on('end', function () {
+            console.log("Body: " + body);
+        });
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end('post received');
+    }
+    else
+    {
+        console.log("GET");
+        //var html = '<html><body><form method="post" action="http://localhost:3000">Name: <input type="text" name="name" /><input type="submit" value="Submit" /></form></body>';
+        //var html = fs.readFileSync('index.html');
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end("hello");
+    }
 }).listen(process.env.PORT || 5000);
 //END AVOIDANCE
 
