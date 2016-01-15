b7748bd3c1cfe9a69e2089f682577cca7440458b
Andrew Eiche <aeiche@gmail.com>
2016-01-15 00:09:38 -0500

    Updating caesar cipher to be correct

--- bot.js
+++ bot.js
@@ -167,18 +167,26 @@ http.createServer(function (req, res) {
 }).listen(process.env.PORT || 5000);
 //END AVOIDANCE
 
-function CaesarCipher(str, num) {
-    str = str.toLowerCase();
-    var result = '';
-    var charcode = 0;
+var caesarShift = function(str, amount) {
+	if (amount < 0)
+		return caesarShift(str, amount + 26);
 
-    for (i = 0; i < str.length; i++) {
-        charcode = (str[i].charCodeAt()) + num;
-        result += String.fromCharCode(charcode);
-    }
-    return result;
+	var output = '';
 
-}
+	for (var i = 0; i < str.length; i ++) {
+		var c = str[i];
+
+		if (c.match(/[a-z]/i)) {
+			var code = str.charCodeAt(i);
+			if ((code >= 65) && (code <= 90))
+				c = String.fromCharCode(((code - 65 + amount) % 26) + 65);
+			else if ((code >= 97) && (code <= 122))
+				c = String.fromCharCode(((code - 97 + amount) % 26) + 97);
+		}
+		output += c;
+	}
+	return output;
+};
 
 
 function getOneAcross(def,cons, cb){
